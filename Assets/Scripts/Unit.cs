924c339019a0074b49ce4404b53b035554370041
Jaroslav Hůna <jaroslav.huna@gmail.com>
2023-02-28 08:23:32 +0100

    Unit Move

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 targetPosition;
+
+
+    private void Update()
     {
-        
+        float stoppingDistance = .1f;
+
+        if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveSpeed * moveDirection * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move(Vector3 targetPosition)
     {
-        
+        this.targetPosition = targetPosition;
+
     }
+
 }
